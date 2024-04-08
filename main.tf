56c652a773d57bf59eaf3efe6feae657973f1f6a
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-04-08 22:45:10 +0300

    First main.tf file added

---
+++ main.tf
@@ -0,0 +1,23 @@
+/**********************************************************
+  # Configure the AWS Provider
+  # Add configuration to authorisation keys
+*/
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+/**********************************************************
+  # Add configuration to authorisation keys
+  # Configure the AWS Provider  
+**********************************************************/
+
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "default"
+}
